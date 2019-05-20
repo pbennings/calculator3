b7e8c3422d998465ab10a9af2f7de125a9be0d10
Paul Bennings <>
2019-05-20 16:54:06 -0400

    finishing up project

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ window.onload = init;
 function init() {
   const oneNumber = document.querySelectorAll(".keyNumbers");
   oneNumber.forEach(eachOne => eachOne.addEventListener("click", doSomething));
+  const grabOps = document.querySelectorAll(".keyOperator");
+  grabOps.forEach(eachOne => eachOne.addEventListener("click", calculate));
+}
+
+function calculate(event) {
+  event.preventDefault();
+  let topSymbols = this.innerText;
+  console.log(topSymbols);
+  calculatorDisplay.innerText = calculate;
+  if (topSymbols === "+") {
+    
+  }
 }
 
 function doSomething(event) {
@@ -11,7 +23,9 @@ function doSomething(event) {
   console.log(displayNum);
   let displayOnScreen = document.querySelector(".calculatorDisplay");
   console.log(typeof displayOnScreen.innerText);
-  if (displayOnScreen.innerText === 0) {
+  if (displayOnScreen.innerText === "0") {
+    displayOnScreen.innerText = displayNum;
   } else {
+    displayOnScreen.innerText += displayNum;
   }
 }
